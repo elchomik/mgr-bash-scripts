703b8d0782509b5d08e1836edeaa8c490b5ddfbb
elChomik <mateuszmarkowski101@wp.pl>
2023-04-10 16:58:28 +0200

    Modified react script

--- badania_react.sh
+++ badania_react.sh
@@ -23,7 +23,7 @@ do
 	then
 		result=$(adb shell top -n 1 -d 1 | grep com.mgr_app | awk '{ printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%.2f,%s,%s\n", $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12 }') 
 		if [ -n "$result" ]; then
-			echo $result >> ~/Desktop/wyniki/wyniki_react/dane_szczegolowe_$1.txt
+			echo $result >> ~/Desktop/wyniki/wyniki_react/dane_usrednione/dane_szczegolowe_$1.txt
 			counter=$((counter+1)) #zwiększ licznik służący do sprawdzania ilości zapisanych rekordów
 		
 			VIRT_col=$(echo $result | awk -F',' '{print $5}' | sed 's/[a-zA-Z]//g') #wyłączenie string z wartości $5
@@ -77,7 +77,12 @@ wynik+="Suma SHR $SHR_sum, Srednia SHR $srednia_SHR \n"
 wynik+="Suma CPU $CPU_sum, Srednia CPU $srednia_CPU \n"
 wynik+="Suma MEM $MEM_sum, Srednia MEM $srednia_MEM \n"
 
+sumy="$VIRT_sum,$RES_sum,$SHR_sum,$CPU_sum,$MEM_sum,$difference"
+srednie="$srednia_VIRT,$srednia_RES,$srednia_SHR,$srednia_CPU,$srednia_MEM,$difference"
+
 echo $wynik >> ~/Desktop/wyniki/wyniki_react/wyniki_react.txt
+echo $sumy >> ~/Desktop/wyniki/wyniki_react/wyniki_react_sumy.txt
+echo $srednie >> ~/Desktop/wyniki/wyniki_react/wyniki_react_srednie.txt
 
 #Uruchomienie skryptu w Pythonie w tle
 nohup python react.py user_$1.csv & 
