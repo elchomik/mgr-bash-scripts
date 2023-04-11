803beb944ddc395327d8bdb751237479836c1d1b
elChomik <mateuszmarkowski101@wp.pl>
2023-04-11 21:00:57 +0200

    Added script which calculates and show average usage of time in sample of data

---
+++ react_wyniki_czas.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print("Skorzystaj z: python react_wyniki.py filename.txt")
+
+path = '~/Desktop/wyniki/wyniki_react/'
+filename = path + sys.argv[1]
+values = ["VIRT", "RES", "SHR", "CPU", "MEM", "TIME", "NR"]
+
+df = pd.read_csv(filename, header=None, names = values)
+
+x_values = df["NR"]
+
+fig1, ax1 = plt.subplots()
+
+ax1.plot(x_values, df["TIME"], label = "TIME [min]")
+
+
+ax1.legend()
+ax1.set_xlabel("Sample number")
+ax1.set_ylabel("Time usage [min]")
+ax1.set_title("Time usage in all sample")
+ax1.set_ylim(ymin=df.min().min(), ymax=df.max().max())
+
+plt.show()
